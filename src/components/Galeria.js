731be869ce86b4f8bc3fc52f08c790ca71da17b1
Jonathan Bidwell <jbidwellb@gmail.com>
2022-10-02 20:00:44 -0300

    contexto

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,11 +1,26 @@
 import "../assets/css/galeria.css";
 import Heart from "./Heart";
+import Context from "../Context";
+import { useContext } from "react";
+import { renderIntoDocument } from "react-dom/test-utils";
 
 export default function Home() {
+  const { fotos } = useContext(Context);
 
+  // AQUÍ VI PARTE DE LO QUE SALÍA EN LA AYUDANTÍA PERO ME HE RENDIDO. 
   return (
     <div className="galeria grid-columns-5 p-3">
+      {fotos.map((foto, i) => (
+        <div
+          onClick={() => setFavorito(foto.id)}
+          className="foto"
+          style={{ backgroundImage: `url(${foto.src})` }}
+          key={i}>
 
+          <Heart filled={foto.favorito} />
+          <p>{foto.descripcion}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/views/Favoritos.js
+++ src/views/Favoritos.js
@@ -1,10 +1,14 @@
+import Context from "../Context";
+import { useContext } from "react";
+
 export default function Favoritos() {
-  
+  const { fotos } = useContext(Context);
+
   return (
     <div>
       <h1>Fotos favoritas</h1>
       <div className="p-3 galeria grid-columns-4">
-       
+
       </div>
     </div>
   );

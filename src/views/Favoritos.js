acb8bc85cfdade9aecfe8cf7ae5fbdc4b7eda813
Jonathan Bidwell <jbidwellb@gmail.com>
2022-10-04 15:02:12 -0300

    comentarios

--- src/views/Favoritos.js
+++ src/views/Favoritos.js
@@ -9,6 +9,7 @@ export default function Favoritos() {
     <div>
       <h1>Fotos favoritas</h1>
       <div className="p-3 galeria grid-columns-4">
+        {/* MAPEA LAS FOTOS FILTRADAS FAVORITAS SOLAMENTE */}
         {fotos.filter((foto) => foto.favorite).map((foto, i) => (
           <img src={foto.src} alt="" key={i} />
         ))}
